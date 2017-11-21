a2f585c39a5ef393b8bc2621228778d09dd0e03b
gbalduzz <gbalduzz@itp.phys.ethz.ch>
2017-11-21 09:09:06 +0100

    Urs version plus update

---
+++ update_main.bash
@@ -0,0 +1,9 @@
+# usage: update_main.bash [dca_build_root]
+DCA_ROOT='../'
+
+if [[ ! -z $1 ]]; then
+    DCA_ROOT=$1
+fi
+
+make -j -C ${DCA_ROOT}/applications/dca && cp ../applications/dca/main_dca ./
+make -j -C ${DCA_ROOT}/applications/analysis && cp ../applications/analysis/main_analysis ./
